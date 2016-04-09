24e3989456087be68ac775dd435fac4a5789ea78
Sergey <sergeypuhl1995@gmail.com>
2016-04-09 12:44:27 +0300

    Remade solution, improve testing

--- intersections/Testing/test.js
+++ intersections/Testing/test.js
@@ -1,158 +1,155 @@
 var examples = {
-  first: [
-    { x: 60,  y: 60  },
-    { x: 180, y: 0   },
-    { x: 300, y: 60  },
-    { x: 300, y: 300 },
-    { x: 240, y: 180 },
-    { x: 210, y: 180 },
-    { x: 180, y: 240 },
-    { x: 150, y: 180 },
-    { x: 120, y: 180 },
-    { x: 60,  y: 300 },
-  ],  
-  second: [
-    { x: 30,  y: 240 },
-    { x: 330, y: 240 },
-    { x: 330, y: 210 },
-    { x: 270, y: 90  },
-    { x: 210, y: 270 },
-    { x: 210, y: 90  },
-    { x: 180, y: 60  },
-    { x: 150, y: 90  },
-    { x: 150, y: 270 },
-    { x: 90,  y: 90  },
-    { x: 30,  y: 210 }
-  ],
+  first: [	[ { x: 60,  y: 60  }, { x: 180, y: 0   }, { x: 300, y: 60  }, { x: 300, y: 300 }, { x: 240, y: 180 }, { x: 210, y: 180 },
+    	   	  { x: 180, y: 240 }, { x: 150, y: 180 }, { x: 120, y: 180 }, { x: 60,  y: 300 } ],
+    	   	
+    	   	[ { x: 30,  y: 30  }, { x: 300, y: 30   }, { x: 300, y: 300  }, { x: 30, y: 300 }, { x: 240, y: 180 } ],
+    	   	
+    	   	[ { x: 60,  y: 60  }, { x: 180, y: 120   }, { x: 300, y: 60  }, { x: 300, y: 300 }, { x: 240, y: 180 }, { x: 210, y: 180 },
+    		  { x: 180, y: 240 }, { x: 150, y: 180 }, { x: 120, y: 180 }, { x: 60,  y: 300 } ],
+
+    		[ { x: 30,  y: 30  }, { x: 60, y: 30   }, { x: 60, y: 60  }, { x: 30, y: 60 }]
+    	   ],
+  second: [	[  { x: 30,  y: 240 }, { x: 330, y: 240 }, { x: 330, y: 210 }, { x: 270, y: 90  }, { x: 210, y: 270 }, { x: 210, y: 90  },
+      		   { x: 180, y: 60  }, { x: 150, y: 90  }, { x: 150, y: 270 }, { x: 90,  y: 90  }, { x: 30,  y: 210 } ],
+      		
+      		[ { x: 0,  y: 150  }, { x: 150, y: 0   }, { x: 150, y: 330  } ],
+
+      		[ { x: 330, y: 240 }, { x: 330, y: 210 }, { x: 270, y: 90  }, { x: 210, y: 270 }, { x: 210, y: 90  }, { x: 180, y: 60  },
+    		  { x: 150, y: 90  }, { x: 160, y: 270 }, { x: 90,  y: 90  } ],
+
+    		[ { x: 60,  y: 30  }, { x: 120, y: 45   }, { x: 60, y: 60  } ]
+      	  ],
   expend:[
-  		[
-	      { x: 60,  y: 240 },
-	      { x: 90,  y: 240 },
-	      { x: 120, y: 180 },
-	      { x: 90,  y: 90  },
-	      { x: 60,  y: 150 },
-	    ],
-	    [
-	      { x: 270, y: 240 },
-	      { x: 300, y: 240 },
-	      { x: 300, y: 150 },
-	      { x: 270, y: 90  },
-	      { x: 240, y: 180 },
-	    ],
-	    [
-	      { x: 150, y: 180 },
-	      { x: 180, y: 240 },
-	      { x: 210, y: 180 },
-	      { x: 210, y: 90  },
-	      { x: 180, y: 60  },
-	      { x: 150, y: 90  }
-	    ]
-  ]
+  			[
+  				[ { x: 60,  y: 240 }, { x: 90,  y: 240 }, { x: 120, y: 180 }, { x: 90,  y: 90  }, { x: 60,  y: 150 }, ],
+  				[ { x: 270, y: 240 }, { x: 300, y: 240 }, { x: 300, y: 150 }, { x: 270, y: 90  }, { x: 240, y: 180 }, ],
+  				[ { x: 150, y: 180 }, { x: 180, y: 240 }, { x: 210, y: 180 }, { x: 210, y: 90  }, { x: 180, y: 60  }, { x: 150, y: 90  } ]
+  			],
 
+  			[
+  				[ { x: 150,  y: 300 }, { x: 125,  y: 300 }, { x: 94.3548, y: 263.2258 }, { x: 150,  y: 231.42857  } ],
+  				[ { x: 82.5,  y: 67.5 }, { x: 150,  y: 115.71428 }, { x: 150, y: 30 }, { x: 120,  y: 30 } ]
+  			],
+
+  			[
+  				[ { x: 300,  y: 150 }, { x: 300,  y: 221.25 }, { x: 242.72727, y: 185.454545 }, { x: 240,  y: 180  }, { x: 270,  y: 90 } ],
+
+  				[ { x: 155.625, y: 191.25 }, { x: 150, y: 180 }, { x: 125, y: 180  }, { x: 90, y: 90 }, { x: 234, y: 180  }, { x: 210, y: 180  },
+    		  	  { x: 210, y: 105  }, { x: 180, y: 120 }, { x: 150.8571,  y: 105.42857  } ]
+  			],
+
+  			[]
+
+  		]
 };
-
 
 describe("intersects", function() {
 
-  it("Find intersections", function() {
-    assert(equalsPolygonsList(examples.expend, intersects(examples.first, examples.second)));
-  });
+    it("Find intersections", function() {
+        assert(equalsPolygonsList(examples.expend[0], intersects(examples.first[0], examples.second[0])));
+    });
+    it("Find intersections", function() {
+        assert(equalsPolygonsList(examples.expend[1], intersects(examples.first[1], examples.second[1])));
+    });
+    it("Find intersections", function() {
+        assert(equalsPolygonsList(examples.expend[2], intersects(examples.first[2], examples.second[2])));
+    });
+    it("Find intersections", function() {
+        assert(equalsPolygonsList(examples.expend[3], intersects(examples.first[3], examples.second[3])));
+    });
 
 });
 
-describe('intersections of segment', function(){
-	it('Seg1 (300;300)(300;60) Seg2(330;240)(30;240)',function(){
-		assert.deepEqual(segmentsIntersect(
-			[{ x: 300, y: 300 },{ x: 300, y: 60 }],
-			[{ x: 330, y: 240 },{ x: 30, y: 240 }]
-			),{x:300, y:240})
-	})
-	it('Seg1 (300;300)(300;60) Seg2(270;90)(330;210)',function(){
-		assert.deepEqual(segmentsIntersect([{ x: 300, y: 300 },{ x: 300, y: 60 }],
-			[{ x: 270, y: 90 },{ x: 330, y: 210 }]
-			),{x:300, y:150})
-	})
-	it('Seg1 (240;108)(300;300) Seg2(330;240)(30;240)',function(){
-		assert.deepEqual(segmentsIntersect(
-			[{ x: 240, y: 180 },{ x: 300, y: 300 }],
-			[{ x: 330, y: 240 },{ x: 30, y: 240 }]
-			),{x:270, y:240})
-	})
-	it('Seg1 (240;180)(300;300) Seg2(240;270)(270;90)',function(){
-		assert.deepEqual(segmentsIntersect(
-			[{ x: 240, y: 180 },{ x: 300, y: 300 }],
-			[{ x: 210, y: 270 },{ x: 270, y: 90 }]
-			),{x:240, y:180})
-	})
-	it('Seg1 (300;300)(300;60) Seg2(330;210)(330;240)',function(){
-		assert.equal(segmentsIntersect(
-			[{ x: 300, y: 300 },{ x: 300, y: 60 }],
-			[{ x: 330, y: 210 },{ x: 330, y: 240 }]
-			),null)
-	})	
+describe('intersections of segments', function() {
+    it('Seg1 (300;300)(300;60) Seg2(330;240)(30;240)', function() {
+        assert.deepEqual(segmentsIntersect(
+            [{ x: 300, y: 300 }, { x: 300, y: 60 }], [{ x: 330, y: 240 }, { x: 30, y: 240 }]
+        ), { x: 300, y: 240 })
+    })
+    it('Seg1 (300;300)(300;60) Seg2(270;90)(330;210)', function() {
+        assert.deepEqual(segmentsIntersect(
+        	[{ x: 300, y: 300 }, { x: 300, y: 60 }], [{ x: 270, y: 90 }, { x: 330, y: 210 }]
+        	), { x: 300, y: 150 })
+    })
+    it('Seg1 (240;108)(300;300) Seg2(330;240)(30;240)', function() {
+        assert.deepEqual(segmentsIntersect(
+            [{ x: 240, y: 180 }, { x: 300, y: 300 }], [{ x: 330, y: 240 }, { x: 30, y: 240 }]
+        ), { x: 270, y: 240 })
+    })
+    it('Seg1 (240;180)(300;300) Seg2(240;270)(270;90)', function() {
+        assert.deepEqual(segmentsIntersect(
+            [{ x: 240, y: 180 }, { x: 300, y: 300 }], [{ x: 210, y: 270 }, { x: 270, y: 90 }]
+        ), { x: 240, y: 180 })
+    })
+    it('Seg1 (300;300)(300;60) Seg2(330;210)(330;240)', function() {
+        assert.equal(segmentsIntersect(
+            [{ x: 300, y: 300 }, { x: 300, y: 60 }], [{ x: 330, y: 210 }, { x: 330, y: 240 }]
+        ), null)
+    })
 })
 
-
-
-
-
-describe('point classify', function(){
-	it('Point (40;40) Seg(30;30)(100;100)',function(){
-		assert.equal(classifyPoint({ x: 40, y: 40}, [{ x: 30, y: 30 },{ x: 100, y: 100 }]), PointClassify.Belong)
-	})
-	it('Point (40;50) Seg(30;30)(100;100)',function(){
-		assert.equal(classifyPoint({ x: 40, y: 50}, [{ x: 30, y: 30 },{ x: 100, y: 100 }]), PointClassify.Left)
-	})
-	it('Point (60;30) Seg(30;30)(100;100)',function(){
-		assert.equal(classifyPoint({ x: 60, y: 30}, [{ x: 30, y: 30 },{ x: 100, y: 100 }]), PointClassify.Right)
-	})
-	it('Point (10;10) Seg(30;30)(100;100)',function(){
-		assert.equal(classifyPoint({ x: 10, y: 10}, [{ x: 30, y: 30 },{ x: 100, y: 100 }]), PointClassify.Behind)
-	})
-	it('Point (110;110) Seg(30;30)(100;100)',function(){
-		assert.equal(classifyPoint({ x: 110, y: 110}, [{ x: 30, y: 30 },{ x: 100, y: 100 }]), PointClassify.Beyond)
-	})
+describe('point classify', function() {
+    it('Point (40;40) Seg(30;30)(100;100)', function() {
+        assert.equal(classifyPoint({ x: 40, y: 40 },
+        	[{ x: 30, y: 30 }, { x: 100, y: 100 }]), PointClassify.Belong)
+    })
+    it('Point (40;50) Seg(30;30)(100;100)', function() {
+        assert.equal(classifyPoint({ x: 40, y: 50 },
+        	[{ x: 30, y: 30 }, { x: 100, y: 100 }]), PointClassify.Left)
+    })
+    it('Point (60;30) Seg(30;30)(100;100)', function() {
+        assert.equal(classifyPoint({ x: 60, y: 30 },
+        	[{ x: 30, y: 30 }, { x: 100, y: 100 }]), PointClassify.Right)
+    })
+    it('Point (10;10) Seg(30;30)(100;100)', function() {
+        assert.equal(classifyPoint({ x: 10, y: 10 },
+        	[{ x: 30, y: 30 }, { x: 100, y: 100 }]), PointClassify.Behind)
+    })
+    it('Point (110;110) Seg(30;30)(100;100)', function() {
+        assert.equal(classifyPoint({ x: 110, y: 110 },
+        	[{ x: 30, y: 30 }, { x: 100, y: 100 }]), PointClassify.Beyond)
+    })
 })
 
-function equalsPolygon(polygon1, polygon2)
-{
-	if(polygon1.length!=polygon2.length) return false;
-	var first = polygon1[0];
-	var offset;
-	var reverse;
-	for (var i =0; i< polygon2.length; i++){
-		if(equalPoints(polygon2[i],first)) {
-			prev = i>0?polygon2[i-1]:polygon2[polygon2.length-1];
-			reverse = equalPoints(polygon1[1],prev);
-			if(reverse){offset=polygon2.length-1-i}
-			else{offset=i;}
-			break
-		}
-	}
-	if(offset == undefined) return false;
-	if(reverse)polygon2=polygon2.reverse();
-	if(offset!=0){
-		var second = polygon2.slice(offset).concat(polygon2.slice(0,offset));
-	}
-	for(var i=0; i<polygon1; i++)
-	{
-		if(!equalPoints(polygon1[i],polygon2[i])) return false;
-	}
-	return true;
+function equalsPolygon(polygon1, polygon2) {
+    if (polygon1.length != polygon2.length) return false;
+    var first = polygon1[0];
+    var offset;
+    var reverse;
+    for (var i = 0; i < polygon2.length; i++) {
+        if (equalPoints(polygon2[i], first)) {
+            prev = i > 0 ? polygon2[i - 1] : polygon2[polygon2.length - 1];
+            reverse = equalPoints(polygon1[1], prev);
+            if (reverse) {
+                offset = polygon2.length - 1 - i
+            } else {
+                offset = i;
+            }
+            break
+        }
+    }
+    if (offset == undefined) return false;
+    if (reverse) polygon2 = polygon2.reverse();
+    if (offset != 0) {
+        var second = polygon2.slice(offset).concat(polygon2.slice(0, offset));
+    }
+    for (var i = 0; i < polygon1; i++) {
+        if (!equalPoints(polygon1[i], polygon2[i])) return false;
+    }
+    return true;
 }
-function equalsPolygonsList(list1, list2)
-{
-	if(list1.length!=list2.length) return false;
-	for(var i=0; i< list1.length; i++)
-	{
-		var found;
-		for (var j=0; j<list2.length;j++){
-			if(equalsPolygon(list1[i],list2[j])){
-				found = true;
-				break
-			}
-		}
-		if(found == undefined) return false;
-	}
-	return true;
+
+function equalsPolygonsList(list1, list2) {
+    if (list1.length != list2.length) return false;
+    for (var i = 0; i < list1.length; i++) {
+        var found;
+        for (var j = 0; j < list2.length; j++) {
+            if (equalsPolygon(list1[i], list2[j])) {
+                found = true;
+                break
+            }
+        }
+        if (found == undefined) return false;
+    }
+    return true;
 }
--- intersections/solution.js
+++ intersections/solution.js
@@ -1,221 +1,188 @@
 function intersects(fig1, fig2) {
-  var intersectionPoints = [];
-  
-  for (var i=0; i< fig1.length; i++)
-  {
-    var segment1 = (i == fig1.length - 1) ? ([fig1[i], fig1[0]] ): ([fig1[i], fig1[i + 1]]);       
-    for (var j=0; j<fig2.length; j++)
-    {
-      var segment2 = (j == fig2.length - 1) ? ([fig2[j], fig2[0]]) : ([fig2[j], fig2[j + 1]]);      
-      var intersectionPoint = segmentsIntersect(segment1,segment2);
-      if(intersectionPoint != null && !contains(intersectionPoints,intersectionPoint)){
-        intersectionPoints.push(intersectionPoint);        
-      }      
-    }    
-  }
-  fig1Segments =[];
-  for (var i=0; i< fig1.length; i++)
-  {    
-    var point = fig1[i];
-    var segment1 = (i == fig1.length - 1) ? ([fig1[i], fig1[0]] ): ([fig1[i], fig1[i + 1]]);
-    fig1Segments.push(segment1);
-    if (inside(point, fig2) && !contains(intersectionPoints,point))
-    {
-      intersectionPoints.push(point);      
-    }
-  }
-  fig2Segments =[];
-  for (var i=0; i< fig2.length; i++)
-  {    
-    var point = fig2[i];
-    var segment2 = (i == fig2.length - 1) ? ([fig2[i], fig2[0]] ): ([fig2[i], fig2[i + 1]]);
-    fig2Segments.push(segment2);
-    if (inside(point, fig1) && !contains(intersectionPoints,point))
-    {
-      intersectionPoints.push(point);      
-    }
-  }
-  resultSegments = [];
+    var intersectionPoints = [];
+    fig1Segments = segmentsOfPolygon(fig1);
+    fig2Segments = segmentsOfPolygon(fig2);
 
-  for (var i=0; i<fig1Segments.length; i++){
-    belongPoints = [];
-    for (var j = 0; j<intersectionPoints.length; j++) {
-      if(classifyPoint(intersectionPoints[j],fig1Segments[i])==PointClassify.Belong)
-      {
-        belongPoints.push(intersectionPoints[j]);        
-      }
-    }
-    if (belongPoints === undefined) continue;
-    if (belongPoints.length > 2){
-      belongPoints.sort(function (a,b){
-        if(a.x!=b.x)
-          return a.x-b.x;
-        else
-          return a.y-b.y;        
-      })
-    }
-    for(var k=0; k<belongPoints.length-1; k++)
-    {     
-      var midP = middlePoint(belongPoints[k],belongPoints[k+1]);        
-      if(inside(midP,fig2))
-      {
-        resultSegments.push([belongPoints[k],belongPoints[k+1]]);          
-      }     
-    }
-  }
-  for (var i=0; i<fig2Segments.length; i++){
-    belongPoints = [];
-    for (var j = 0; j<intersectionPoints.length; j++) {
-      if(classifyPoint(intersectionPoints[j],fig2Segments[i])==PointClassify.Belong)
-      {
-        belongPoints.push(intersectionPoints[j]);        
-      }
-    }
-    if (belongPoints === undefined) continue;
-    if (belongPoints.length > 2){
-      belongPoints.sort(function (a,b){
-        if(a.x!=b.x)
-          return a.x-b.x;
-        else
-          return a.y-b.y;        
-      })
-    }
-    for(var k=0; k<belongPoints.length-1; k++)
-    {     
-      var midP = middlePoint(belongPoints[k],belongPoints[k+1]);        
-      if(inside(midP,fig1))
-      {
-        resultSegments.push([belongPoints[k],belongPoints[k+1]]);          
-      }     
-    }
-  }
 
-  var polygons = []
-
-  for (var i=0; i<resultSegments.length-1; i++)
-  {
-    if(resultSegments[i]==null) continue;
-    var polygon = [];
-    polygon.push(resultSegments[i][0]);
-    polygon.push(resultSegments[i][1]);
-    point = resultSegments[i][1];
-    resultSegments[i]=null;
-    for(var j=i+1; j<resultSegments.length; j++)
-    {
-      if(resultSegments[j]==null) continue;
-      if(equalPoints(point,resultSegments[j][0]))
-      {
-        point = resultSegments[j][1];        
-      }
-      else if(equalPoints(point,resultSegments[j][1]))
-      {
-        point = resultSegments[j][0];        
-      }
-      else
-      {
-        continue;
-      }
-      resultSegments[j]=null;
-      if(equalPoints(polygon[0],point)) break;
-      polygon.push(point)
-      j=i;
+    for (var i = 0; i < fig1Segments.length; i++) {
+        for (var j = 0; j < fig2Segments.length; j++) {
+            var intersectionPoint = segmentsIntersect(fig1Segments[i], fig2Segments[j]);
+            if (intersectionPoint != null && !contains(intersectionPoints, intersectionPoint)) {
+                intersectionPoints.push(intersectionPoint);
+            }
+        }
     }
-    if(polygon.length>2){    polygons.push(polygon);}
-  }
 
-  return polygons;
+    pointsInsidePolygon(fig1, fig2Segments, intersectionPoints);
+    pointsInsidePolygon(fig2, fig1Segments, intersectionPoints);
+
+    resultSegments = [];
+    getResultSegments(fig1Segments, intersectionPoints, fig2Segments, resultSegments);
+    getResultSegments(fig2Segments, intersectionPoints, fig1Segments, resultSegments);
+
+    var polygons = [];
+    for (var i = 0; i < resultSegments.length - 1; i++) {
+        if (resultSegments[i] == null) continue;
+        var polygon = [];
+        polygon.push(resultSegments[i][0]);
+        polygon.push(resultSegments[i][1]);
+        point = resultSegments[i][1];
+        resultSegments[i] = null;
+        for (var j = i + 1; j < resultSegments.length; j++) {
+            if (resultSegments[j] == null) continue;
+            if (equalPoints(point, resultSegments[j][0])) {
+                point = resultSegments[j][1];
+            } else if (equalPoints(point, resultSegments[j][1])) {
+                point = resultSegments[j][0];
+            } else {
+                continue;
+            }
+            resultSegments[j] = null;
+            if (equalPoints(polygon[0], point)) break;
+            polygon.push(point)
+            j = i;
+        }
+        if (polygon.length > 2) {
+            polygons.push(polygon);
+        }
+    }
+
+    return polygons;
 }
 
 var PointClassify = {
-  Right : {value: 0, name: "Right"}, 
-  Left: {value: 1, name: "Left"}, 
-  Belong : {value: 2, name: "Belong"},
-  Behind : {value: 3, name: "Behind"},
-  Beyond : {value: 4, name: "Beyond"}
+    Right: { value: 0, name: "Right" },
+    Left: { value: 1, name: "Left" },
+    Belong: { value: 2, name: "Belong" },
+    Behind: { value: 3, name: "Behind" },
+    Beyond: { value: 4, name: "Beyond" }
 };
 
 var SegmentType = {
-  Crossing : {value:0, name: "Crossing"},
-  Inessential : {value:1, name: "Inessential"}
+    Crossing: { value: 0, name: "Crossing" },
+    Inessential: { value: 1, name: "Inessential" }
 };
 
 function contains(a, obj) {
     var i = a.length;
     while (i--) {
-       if (a[i] === obj) {
-           return true;
-       }
-       if(equalPoints(a[i], obj))
-       {
+        if (a[i] === obj) {
             return true;
-       }
+        }
+        if (equalPoints(a[i], obj)) {
+            return true;
+        }
     }
     return false;
 }
-function equalPoints(point1, point2)
-{
-  if (Math.abs(point1.x-point2.x)<0.0001 && Math.abs(point1.y-point2.y)<0.0001)return true;
+
+function equalPoints(point1, point2) {
+    if (Math.abs(point1.x - point2.x) < 0.0001 && Math.abs(point1.y - point2.y) < 0.0001) return true;
 }
 
-function middlePoint(point1, point2)
-{
-  return {x:(point1.x+point2.x)/2, y:(point1.y+point2.y)/2};
+function middlePoint(point1, point2) {
+    return {
+        x: (point1.x + point2.x) / 2,
+        y: (point1.y + point2.y) / 2
+    };
 }
 
-
-
-function segmentsIntersect(segment1, segment2)
-{
-  denominator = (segment2[1].y - segment2[0].y) * (segment1[1].x - segment1[0].x) -
-                (segment2[1].x - segment2[0].x) * (segment1[1].y - segment1[0].y);
-  if(Math.abs(denominator)<0.00001) return null;
-  t1 = ((segment2[1].x - segment2[0].x) * (segment1[0].y - segment2[0].y) -
+function segmentsIntersect(segment1, segment2) {
+    denominator = (segment2[1].y - segment2[0].y) * (segment1[1].x - segment1[0].x) -
+        (segment2[1].x - segment2[0].x) * (segment1[1].y - segment1[0].y);
+    if (Math.abs(denominator) < 0.00001) return null;
+    t1 = ((segment2[1].x - segment2[0].x) * (segment1[0].y - segment2[0].y) -
         (segment2[1].y - segment2[0].y) * (segment1[0].x - segment2[0].x)) / denominator;
-  if(t1<0 || t1>1) return null;
-  t2 = ((segment1[1].x - segment1[0].x) * (segment1[0].y - segment2[0].y) -
+    if (t1 < 0 || t1 > 1) return null;
+    t2 = ((segment1[1].x - segment1[0].x) * (segment1[0].y - segment2[0].y) -
         (segment1[1].y - segment1[0].y) * (segment1[0].x - segment2[0].x)) / denominator;
-  if(t2<0 || t2>1) return null;
+    if (t2 < 0 || t2 > 1) return null;
 
-  return {
-            x:segment1[0].x + t1 * (segment1[1].x - segment1[0].x),
-            y:segment1[0].y + t1 * (segment1[1].y - segment1[0].y)
-          };
+    return {
+        x: segment1[0].x + t1 * (segment1[1].x - segment1[0].x),
+        y: segment1[0].y + t1 * (segment1[1].y - segment1[0].y)
+    };
 }
 
-function classifyPoint(point, segment)
-{
-  var a = {x:(segment[1].x-segment[0].x), y:(segment[1].y-segment[0].y)};
-  var b = {x:(point.x-segment[0].x), y:(point.y-segment[0].y)};
-  var areaDif = a.x*b.y - b.x*a.y;
-  if(equalPoints(segment[0], point)) return PointClassify.Belong;
-  if(equalPoints(segment[1], point)) return PointClassify.Belong;
-  if(areaDif>0.00001) return PointClassify.Left;
-  if(areaDif<-0.00001) return PointClassify.Right;
-  if(a.x*b.x < 0 || a.y*b.y < 0) return PointClassify.Behind;
-  if(Math.sqrt(a.x*a.x+a.y*a.y)<Math.sqrt(b.x*b.x+b.y*b.y)) return PointClassify.Beyond;
-  return PointClassify.Belong;
+function classifyPoint(point, segment) {
+    var a = {
+        x: (segment[1].x - segment[0].x),
+        y: (segment[1].y - segment[0].y)
+    };
+    var b = {
+        x: (point.x - segment[0].x),
+        y: (point.y - segment[0].y)
+    };
+    var areaDif = a.x * b.y - b.x * a.y;
+    if (equalPoints(segment[0], point)) return PointClassify.Belong;
+    if (equalPoints(segment[1], point)) return PointClassify.Belong;
+    if (areaDif > 0.00001) return PointClassify.Left;
+    if (areaDif < -0.00001) return PointClassify.Right;
+    if (a.x * b.x < 0 || a.y * b.y < 0) return PointClassify.Behind;
+    if (Math.sqrt(a.x * a.x + a.y * a.y) < Math.sqrt(b.x * b.x + b.y * b.y)) return PointClassify.Beyond;
+    return PointClassify.Belong;
 }
 
-function getSegmentType(point, segment)
-{
-  switch(classifyPoint(point,segment)){
-    case PointClassify.Right:
-      return segment[0].x < point.x && point.x <= segment[1].x ? SegmentType.Crossing : SegmentType.Inessential;
-    case PointClassify.Left:
-      return segment[1].x < point.x && point.x <= segment[0].x ? SegmentType.Crossing : SegmentType.Inessential;
-    default:
-      return SegmentType.Inessential;
-  }
-}
-
-function inside(point, polygon)
-{
-  var parity = 0;
-    for (var j=0; j<polygon.length; j++)
-    {
-      var segment = (j == polygon.length - 1) ? ([polygon[j], polygon[0]]) : ([polygon[j], polygon[j + 1]]);
-      if (getSegmentType(point, segment) == SegmentType.Crossing) { parity = 1 - parity; }
+function getSegmentType(point, segment) {
+    switch (classifyPoint(point, segment)) {
+        case PointClassify.Right:
+            return segment[0].x < point.x && point.x <= segment[1].x ? SegmentType.Crossing : SegmentType.Inessential;
+        case PointClassify.Left:
+            return segment[1].x < point.x && point.x <= segment[0].x ? SegmentType.Crossing : SegmentType.Inessential;
+        default:
+            return SegmentType.Inessential;
     }
-    return parity===1;
 }
 
+function inside(point, polygon) {
+    var parity = 0;
+    for (var i = 0; i < polygon.length; i++) {
+        if (getSegmentType(point, polygon[i]) == SegmentType.Crossing) {
+            parity = 1 - parity;
+        }
+    }
+    return parity === 1;
+}
+
+function segmentsOfPolygon(points) {
+    var segments = []
+    for (var i = 0; i < points.length; i++) {
+        segments.push(i == points.length - 1 ? [points[i], points[0]] : [points[i], points[i + 1]]);
+    }
+    return segments;
+}
+
+function pointsInsidePolygon(points, polygon, resultPoints) {
+    for (var i = 0; i < points.length; i++) {
+        var point = points[i];
+        if (inside(point, polygon) && !contains(resultPoints, point)) {
+            resultPoints.push(point);
+        }
+    }
+}
+
+function getResultSegments(fig1Segments, intersectionPoints, fig2Segments, resultSegments) {
+    for (var i = 0; i < fig1Segments.length; i++) {
+        belongPoints = [];
+        for (var j = 0; j < intersectionPoints.length; j++) {
+            if (classifyPoint(intersectionPoints[j], fig1Segments[i]) == PointClassify.Belong) {
+                belongPoints.push(intersectionPoints[j]);
+            }
+        }
+        if (belongPoints === undefined) continue;
+        if (belongPoints.length > 2) {
+            belongPoints.sort(function(a, b) {
+                if (a.x != b.x)
+                    return a.x - b.x;
+                else
+                    return a.y - b.y;
+            })
+        }
+        for (var k = 0; k < belongPoints.length - 1; k++) {
+            var midP = middlePoint(belongPoints[k], belongPoints[k + 1]);
+            if (inside(midP, fig2Segments)) {
+                resultSegments.push([belongPoints[k], belongPoints[k + 1]]);
+            }
+        }
+    }
+}
